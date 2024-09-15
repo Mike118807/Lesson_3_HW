deb712d7124476c9a8b6b05ff45ec2d32257bcf5
Michael Davis <165938327+Mike118807@users.noreply.github.com>
2024-09-14 17:40:00 -0700

    Add files via upload

---
+++ lesson3_#1_tsk2.py
@@ -0,0 +1,8 @@
+# Task 2: Calculate the average grade and print it.
+
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+
+av_grade= sum(grades) / len(grades)
+
+print(av_grade)
---
+++ lesson3_#2_tsk1.py
@@ -0,0 +1,16 @@
+# Task 1: Given the two lists:
+#
+# submitted = ["Alice", "Bob", "Charlie", "David"]
+# attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+
+if "Alice" in submitted and "Alice" in attended:
+
+    print ("Alice submitted her assignment and attended class ")
+
+else:
+    print ("Alice did not submit her assignment or did not attend class ")
